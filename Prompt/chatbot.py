de0673bf21fc93bdf8e7743743c81e82bb816f2d
Ahmad Pervaiz <ahmad.pervaiz2212@gmail.com>
2026-07-04 19:59:54 +0500

    Add file: chatbot.py

---
+++ Prompt/chatbot.py
@@ -0,0 +1,26 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOllama(
+    temperature=0.7,
+    model="llama3.2"
+)
+
+
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant')
+]
+
+while True:
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+
+print(chat_history)
