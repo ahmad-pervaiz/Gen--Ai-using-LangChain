f6bb96398ddeebee7b88fc915d666681b31b5b1e
Ahmad Pervaiz <ahmad.pervaiz2212@gmail.com>
2026-07-05 20:25:24 +0500

    Add file: with_structure_otp_pydantic.py

---
+++ Structure_output/with_structure_otp_pydantic.py
@@ -0,0 +1,52 @@
+from typing import TypedDict,Annotated,Optional
+from langchain_ollama import ChatOllama
+from pydantic import Field, BaseModel
+
+model=ChatOllama(
+    temperature=0.7,
+    model="llama3.2"
+)
+
+class Review(BaseModel):
+    key_themes: list[str] = Field(description="Write down all the key themes discussed in the review in a list")
+    summary: str = Field(description="Short summary of the review")
+    sentiment: str = Field(description="Sentiment: positive, negative, or neutral")
+    pros: Optional[list[str]] = Field(default=None, description="Write down all the prosinside a list")
+    cons: Optional[list[str]] = Field(default=None, description="Write down all the consinside a list")
+    name: Optional[str] = Field(default=None, description="Write the name of the reviewer")
+    
+structured_model=model.with_structured_output(Review)
+
+review="""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it's an absolute powerhouse! The Snapdragon 8 Gen 3
+processor makes everything lightning fast-whether I'm gaming, multitasking, or editing photos. The 5000mAh battery easily
+lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me
+away is the 200MP camera-the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x
+actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung's One UI still comes with
+bloatware-why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard
+pill to swallow.
+
+Pros :
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+
+Cons:
+Bulky and heavy-not great for one-handed use
+Bloatware still exists in One UI
+Expensive compared to competitors I"""
+result=structured_model.invoke(review)
+
+# Print nicely
+print(result)
+print("\n" + "="*50)
+print("Key Themes:", result.key_themes)
+print("Summary:", result.summary)
+print("Sentiment:", result.sentiment)
+print("Pros:", result.pros)
+print("Cons:", result.cons)
+print("Reviewer Name:", result.reviewer_name)
